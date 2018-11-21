0b798625628b44d3fabebf52bd781be6bae8b323
Michael Banzon <mrnbanzon@gmail.com>
2018-11-21 07:38:51 -0800

    Initialize configuration for webpack.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: [/src/],
+        exclude: [/node_modules/],
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+};
